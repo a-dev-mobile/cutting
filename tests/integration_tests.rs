d253557f56ce95fec58fc0a729e0054991c3a5dd
Trofimov Dmitriy <wayofdt@gmail.com>
2025-06-21 18:39:15 +0300

    refactor: Move enums module re-export to integration_tests; remove redundant lib.rs file

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,8 +1,9 @@
 mod models;
 mod utils;
 mod stock;
-
+pub mod enums;
 // Re-export test modules for easier access
 pub use models::*;
 pub use utils::*;
 pub use stock::*;
+pub use enums::*;
--- tests/lib.rs
+++
@@ -1,4 +0,0 @@
-pub mod enums;
-pub mod models;
-pub mod stock;
-pub mod utils;
